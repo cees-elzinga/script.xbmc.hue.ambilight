e53833070b2d8c3c1f06a9ae6e67608c638b93c3
Cees <cees@Ceess-Mac-mini.local>
2013-03-04 20:52:01 +0100

    Turn the lights back on when playback ends (thanks Bruno)

--- default.py
+++ default.py
@@ -66,6 +66,9 @@ class MyPlayer(xbmc.Player):
   def onPlayBackStopped(self):
     state_changed("stopped")
 
+  def onPlayBackEnded(self):
+    state_changed("stopped")
+
 class Hue:
   params = None
   connected = None
